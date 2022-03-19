5cb4004ab6011f77229b0f22a6a1206fb50ec424
0xzen <team@mahj.vip>
2022-03-19 14:50:36 +0800

    add MahjongDAOAirdrop contract

---
+++ scripts/deploy_mahjong_airdrop.js
@@ -0,0 +1,36 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+
+  // We get the contract to deploy
+  const Airdrop = await hre.ethers.getContractFactory("MahjongDAOAirdrop");
+  const airdrop = await Airdrop.deploy("0x5FbDB2315678afecb367f032d93F642f64180aa3");
+
+  await airdrop.deployed();
+
+  console.log("Airdrop deployed to:", airdrop.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
--- test/mahjong.js
+++ test/mahjong.js
@@ -1,24 +1,53 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Mahjong", function () {
-  it("Should return the new greeting once it's changed", async function () {
+describe("MahjongDAO", function () {
+  it("Should work", async function () {
     const accounts = await ethers.getSigners();
 
-    for (const account of accounts) {
-      console.log(account.address);
-    }
-    const Mahjong = await ethers.getContractFactory("Mahjong");
-    const mahjong = await Mahjong.deploy();
+    const MahjongDAO = await ethers.getContractFactory("MahjongDAO");
+    const mahjong = await MahjongDAO.deploy();
     await mahjong.deployed();
 
-    expect(await mahjong.greet()).to.equal("Hello, world!");
+    expect(await mahjong['name()']()).to.equal('Mahjong DAO Tokens');
+    expect(await mahjong['symbol()']()).to.equal('MAHJ');
+    expect(await mahjong['balanceOf(address,uint256)'](accounts[0].address, 1)).to.equal(0);
 
-    const setGreetingTx = await mahjong.setGreeting("Hola, mundo!");
+    await mahjong['mint(address,uint256,uint256,bytes)'](accounts[0].address, 1, 1024, '0x');
+    expect(await mahjong['balanceOf(address,uint256)'](accounts[0].address, 1)).to.equal(1024);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    try {
+      await mahjong['safeTransferFrom(address,address,uint256,uint256,bytes)'](accounts[0].address, mahjong.address, 1, 1024, '0x');
+      expect(false).to.equal(true);
+    } catch (err) {
+      expect(err.message).to.contain("reverted with reason string 'non ERC1155Receiver'")
+    }
 
-    expect(await mahjong.greet()).to.equal("Hola, mundo!");
+    const Airdrop = await hre.ethers.getContractFactory("MahjongDAOAirdrop");
+    const airdrop = await Airdrop.deploy(mahjong.address);
+    await airdrop.deployed();
+
+    expect(await mahjong['balanceOf(address,uint256)'](airdrop.address, 1)).to.equal(0);
+    await mahjong['safeTransferFrom(address,address,uint256,uint256,bytes)'](accounts[0].address, airdrop.address, 1, 1024, '0x');
+    expect(await mahjong['balanceOf(address,uint256)'](airdrop.address, 1)).to.equal(1024);
+
+    try {
+      await airdrop['claim(uint256)'](1);
+      expect(false).to.equal(true);
+    } catch (err) {
+      expect(err.message).to.contain("reverted with reason string 'airdrop not exists'")
+    }
+
+    await airdrop['setAirdrop(uint256,uint256)'](1, 100);
+    await airdrop['claim(uint256)'](1);
+    expect(await mahjong['balanceOf(address,uint256)'](airdrop.address, 1)).to.equal(924);
+    expect(await mahjong['balanceOf(address,uint256)'](accounts[0].address, 1)).to.equal(100);
+
+    try {
+      await airdrop['claim(uint256)'](1);
+      expect(false).to.equal(true);
+    } catch (err) {
+      expect(err.message).to.contain("reverted with reason string 'condition not met'")
+    }
   });
 });
